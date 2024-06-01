65a8c82e32011298c8a33d8ff36e5ee8ed1879a0
Keshav Varadarajan <keshav.varadarajan@duke.edu>
2024-05-31 20:05:30 -0400

    Changed email to id

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -12,7 +12,7 @@ const router = express.Router();
 
 router.get('/', getUsers);
 router.post('/new', createUser);
-router.get('/user/:email', getUser);
+router.get('/user/:id', getUser);
 router.put('/update/:id', updateUser);
 router.delete('/delete/:id', deleteUser);
 
