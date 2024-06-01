c97b5e0ae736402826865741033e1ceefeb7bd76
Keshav Varadarajan <keshav.varadarajan@duke.edu>
2024-05-31 22:58:45 -0400

    Added video embedding for practicals.

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Register from './components/accounts/Register';
 import Login from './components/accounts/Login';
-import Home from './components/Home';
+import Home from './components/pages/Home';
 import { AuthProvider } from './contexts/AuthContext';
 import ErrorMessage from './components/layouts/ErrorMessage';
 import Header from './components/layouts/Header';
 import WithPrivateRoute from './utils/WithPrivateRoute';
+import Practical from './components/pages/Practical';
 
 function App() {
   return (
@@ -18,6 +19,8 @@ function App() {
           <Route exact path="/" element={<Home />} />
           <Route exact path="/register" element={<Register />} />
           <Route exact path="/login" element={<Login />} />
+          <Route exact path="/practical/:id" element={<Practical />} />
+
         </Routes>
       </Router>
     </AuthProvider>
--- client/src/components/pages/Practical.js
+++ client/src/components/pages/Practical.js
@@ -1,9 +1,82 @@
-import React from 'react'
 
 import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
+
+import YouTube from "react-youtube"
+import { Typography, Box } from "@mui/material"
 
 export default function Practical() {
+
+
+    const params = useParams();
+    const id = params.id
+
+    /*
+
+    (this.video_link = video_link),
+            (this.user_creator = user_creator),
+            (this.user_participants = user_participants),
+            (this.user_instructors = user_instructors),
+            (this.comments = comments),
+            (this.chats = chats);
+            */
+
+
+    const [videoId, setVideoId] = useState("")
+    const [userCreator, setUserCreator] = useState(null)
+    const [userParticipants, setUserParticipants] = useState([])
+    const [userInstructors, setUserInstructors] = useState([])
+
+    useEffect(() => {
+        async function fetchPractical() {
+
+            const practical_res = await fetch(`http://localhost:3001/api/practical/${id}`);
+            const practical = await practical_res.json()
+
+            const videoParams = practical.video_link.split("/")
+            setVideoId(videoParams[videoParams.length - 1])
+
+            const user_creator_id = practical.user_creator
+            const creator_res = await fetch(`http://localhost:3001/api/user/${user_creator_id}`);
+            const userCreator = await creator_res.json()
+            setUserCreator(userCreator)
+        }
+
+        fetchPractical()
+    }, [])
+
+    console.log(videoId)
+
     return (
-        <div>Practical</div>
+        <Box sx={{
+            minHeight: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            py: 12,
+            px: 4,
+            flexDirection: "column"
+        }}>
+            <Box sx={{
+                my: 5,
+                width: "100%"
+            }}>
+                <Typography variant="h3"> Practical </Typography>
+            </Box>
+
+            <Box sx={{
+                display: "flex",
+            }}>
+                <YouTube videoId={videoId} />
+                <Box sx={{
+                    backgroundColor: "red"
+                }}>
+                    <Typography>
+                        Grading
+                    </Typography>
+                </Box>
+            </Box>
+
+        </Box>
     )
 }
