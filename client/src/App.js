be1a7a4f79079e5f590628d40d37817a6e3d8b76
Jonathan Hyun <jonathan.hyun@duke.edu>
2024-05-31 23:08:49 -0400

    push

--- client/src/App.js
+++ client/src/App.js
@@ -14,15 +14,12 @@ import NavBar from './components/elements/NavBar';
 
 
 
-
-
 function App() {
   return (
     <AuthProvider>
       <Router>
         <Header />
         <Box sx={{ display: 'flex' }}>
-<<<<<<< HEAD
         <NavBar/>
         <ErrorMessage />
         <Routes>
@@ -30,42 +27,6 @@ function App() {
           <Route exact path="/register" element={<Register />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/practical/:id" element={<Practical />} />
-=======
-          <Drawer sx={{
-            width: drawerWidth,
-            flexShrink: 0,
-            '& .MuiDrawer-paper': {
-              width: drawerWidth,
-              boxSizing: 'border-box',
-            },
-          }}
-            variant="permanent"
-            anchor="left">
-            <Toolbar />
-            <Divider />
-            <List>
-              {/* {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                        <ListItem key={text} disablePadding>
-                        <ListItemButton>
-                            <ListItemIcon>
-                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                            </ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItemButton>
-                        </ListItem>
-                    ))} */}
-            </List>
-            <Divider />
->>>>>>> 5338b668c4d9e165bde3831c65eecf9d24b51075
-
-          </Drawer>
-          <ErrorMessage />
-          <Routes>
-            <Route exact path="/" element={<Dashboard />} />
-            <Route exact path="/register" element={<Register />} />
-            <Route exact path="/login" element={<Login />} />
-            <Route exact path="/practical/:id" element={<Practical />} />
-
           </Routes>
         </Box>
       </Router>
