e8a2e46295bfe2c4b7a18c02a72b26dd9972629d
Jonathan Hyun <jonathan.hyun@duke.edu>
2024-09-26 01:02:46 -0400

    user_pages

--- client/src/components/pages/AdminDashboard.js
+++ client/src/components/pages/AdminDashboard.js
@@ -497,11 +497,11 @@ export default function AdminDashboard() {
                     top: "50%",
                     left: "50%",
                     transform: "translate(-50%, -50%)",
-                    width: 800,
+                    width: 400,
                     bgcolor: "background.paper",
-                    border: "2px solid #000",
                     boxShadow: 24,
                     p: 4,
+                    borderRadius:"10px",
                 }}>
 
                     <Box sx={{
